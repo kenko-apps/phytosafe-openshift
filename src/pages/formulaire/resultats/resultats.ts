1f8a92114dd1bfd8f125533d3229e240abfbef40
callio <cedricallio@yahoo.fr>
2017-08-23 23:50:20 +0200

    Finalisation Nom Phyto

--- src/pages/formulaire/resultats/resultats.ts
+++ src/pages/formulaire/resultats/resultats.ts
@@ -14,7 +14,7 @@ import { LocalStockage } from '../../../providers/localstockage';
 export class Resultats {
 
   constructor(public navCtrl: NavController, public translate: TranslateService, public formulaire: Formulaire, public localstockage: LocalStockage) {
-    traitement.getTrait().toPromise().then((res) => {
+    /*traitement.getTrait().toPromise().then((res) => {
       this.traitementNom = [res.blob];//A VERIFIER - BLOB PERMET DE RETROUVER LE BODY DE LA REPONSE!!
     }).catch((err)=>{
       console.error('ERROR', err);
